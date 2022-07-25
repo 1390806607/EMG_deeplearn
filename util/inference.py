cebc9d58db34504c7aff5e8c1e26df93065dfc11
Administrator <1390806607@qq.com>
2022-07-25 10:02:04 +0800

    commit

--- util/inference.py
+++ util/inference.py
@@ -35,22 +35,45 @@ def get_data(path):
         data.append(value)
         label.append([label_to_id[key[:-1]]]*value.shape[0])
     # data = np.array(data).reshape((-1,3000,2))
-    data = np.array(data).reshape(-1, 2, 30, 3000).transpose(0, 2, 3, 1).reshape(-1, 3000, 2)
-    label = np.max(np.array(label).reshape(-1, 2, 30).transpose(0, 2, 1).reshape(-1, 2), axis=1)
+    # data = np.array(data).reshape(-1, 2, 30, 3000).transpose(0, 2, 3, 1).reshape(-1, 3000, 2)
+    # label = np.max(np.array(label).reshape(-1, 2, 30).transpose(0, 2, 1).reshape(-1, 2), axis=1)
+    data = np.array(data).reshape(-1, 2, 1, 3000).transpose(0, 2, 3, 1).reshape(-1, 3000, 2)
+    label = np.max(np.array(label).reshape(-1, 2, 1).transpose(0, 2, 1).reshape(-1, 2), axis=1)
     # print(label_to_id)
     # print(data.shape)
     # print(label.shape)
     return data, label, label_to_id, id_to_label
 
+# 从txt中读取二通道文件
+def getdata_from_txt(path,labels_path):
+
+    id2label = {}
+    tmp_data = []
+    f = open(path, 'r', encoding='utf-8')
+    for line in f.readlines():
+        if len(line.strip().split(',')) == 2 and line.strip() != '':
+            res = line.strip().split(',')
+            try:
+                res = list(map(lambda x: int(x), res))
+            except ValueError as e:
+                continue
+            tmp_data.append(res)
+    tmp_data = np.array(tmp_data, dtype=np.int32)
+    tmp_data = np.array(list(map(lambda a: a / np.max(a), tmp_data)))
+
+    with open(labels_path, 'r', encoding='utf-8') as f1:
+        for index,value in enumerate(f1.readlines()):
+            id2label[index] = value
+        f1.close()
+    return tmp_data, id2label
 
 
-
-def predict(data,target,id2label,infeature,input_channels,num_classes,flag=True,timeWindow = 40,strideWindow = 40):
+def predict(data,id2label,infeature,input_channels,num_classes,flag=True,timeWindow = 40,strideWindow = 40):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print("Device being used:", device)
     # model = DNN(in_features=infeature,classes_num=num_classes)
     model = CNN(input_channels, num_classes)
-    checkpoint = torch.load('../models/epoch-1999.pth.tar',
+    checkpoint = torch.load('../models/epoch-199.pth.tar',
                             map_location=lambda storage, loc: storage)
     model.load_state_dict(checkpoint['state_dict'])  # 模型参数
     # optimizer.load_state_dict(checkpoint['opt_dict'])#优化参数
@@ -58,11 +81,12 @@ def predict(data,target,id2label,infeature,input_channels,num_classes,flag=True,
     model.to(device)
     model.eval()
 
-    index_list = np.random.permutation(data.shape[0])   # (batch_size,3000,2)
-
-
-    input_  = data[index_list[0]]           # (3000,2)
-    label = target[index_list[0]]
+    # index_list = np.random.permutation(data.shape[0])   # (batch_size,3000,2)
+    #
+    #
+    # input_  = data[index_list[0]]           # (3000,2)
+    input_ = data
+    # label = target[index_list[0]]
     if flag:
         length = math.floor((input_.shape[0] - timeWindow) / strideWindow)
         temp = []   # 临时变量
@@ -79,9 +103,10 @@ def predict(data,target,id2label,infeature,input_channels,num_classes,flag=True,
 
 
     clip = []
+    count = {}
     for value in input_:
         clip.append(value)
-        if len(clip) == 40:
+        if len(clip) == 20:
             inputs = np.array(clip).astype(np.float32)
             # inputs = np.expand_dims(inputs, axis=0).reshape(-1,infeature)
             inputs = np.expand_dims(np.expand_dims(inputs, axis=0), axis=0)
@@ -90,11 +115,17 @@ def predict(data,target,id2label,infeature,input_channels,num_classes,flag=True,
             start_time = time.time()
             with torch.no_grad():
                 outputs = model.forward(inputs)
-            print(f'time:{time.time()-start_time}')
+            # print(f'time:{time.time()-start_time}')
             probs = torch.nn.Sigmoid()(outputs)
             predict_label = torch.max(probs, 1)[1].detach().cpu().numpy()[0]
-            print(id2label[predict_label],id2label[label])
+            print(id2label[predict_label])
+            if id2label[predict_label] not in count.keys():
+                count[id2label[predict_label]] = 0
+            else:
+                count[id2label[predict_label]] += 1
             clip.pop(0)
+    print(count)
+    print(max(count, key=count.get))
 
 
 
@@ -107,7 +138,8 @@ if __name__=='__main__':
     input_size = 400
     input_channels = 1
     hidden_size = 256
-    num_classes = 6
-    path = '../data/sEMG_for_Basic_Hand_movements/Database 1/female_2.mat'
-    data, label, label_to_id, id_to_label = get_data(path)
-    predict(data,label,id_to_label,input_size,input_channels,num_classes)
+    num_classes = 7
+    path = '../data/mydataset/1_2/palmar5.txt'
+    labels_path = '../data/mydataset/labels.txt'
+    data, id_to_label = getdata_from_txt(path, labels_path)
+    predict(data,id_to_label,input_size,input_channels,num_classes)
